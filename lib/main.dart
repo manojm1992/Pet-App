f2579a8f4678d9abb80a53fe33dadfa9cfd4bdd7
Manoj <157724006+manojm1992@users.noreply.github.com>
2024-03-27 11:58:37 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,28 @@
+
+
+
 import 'package:flutter/material.dart';
 
-import 'oe_page.dart';
+
+import 'pages/onboard.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
-      home:   HomePage(),
+      home: const OnBoardPage(),
     );
   }
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pet_shop_app_01/main.dart';
 
-import 'package:flutter_application_1/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
